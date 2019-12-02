20e80a2b239633b9e79ee624148fd1f679d50c42
diego <diegodamibenitez@gmail.com>
2019-12-01 23:41:57 -0300

    aver si anda esto

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,5 +3,4 @@ from .models import Project
 
 def portfolio(request):
     projects = Project.objects.all()
-    print(type(projects))
     return render(request, "portfolio/portfolio.html", {'projects':projects})
